3ed975e2ad14ab22f9669df37a0c387d1f90f7ad
Alejandro <espejovacafloresjuanalejandro@gmail.com>
2021-04-15 19:04:55 -0400

    Problema_Ocho

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-var string1 = "aabcc";
-var string2 = "adcaa";
-var array = [];
-var count = 0;
-for (var i = 0; i < string2.length; i++)
-    array[i] = string2.charAt(i);
-for (var i = 0; i < string1.length; i++)
-    for (var j = 0; j < array.length; j++)
-        if (string1.charAt(i) == array[j] && array[j] != "") {
-            count++;
-            array[j] = "";
-            break;
-        }
-console.log(count);
+var number = 9;
+var count = 1;
+var count2 = 2;
+while (count <= number) {
+    var aux = 0;
+    for (var i = 1; i <= count2; i++)
+        if (count2 % i == 0)
+            aux++;
+    if (aux == 2) {
+        console.log(count2);
+        count++;
+    }
+    count2++;
+}
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-var string1 = "aabcc";
-var string2 = "adcaa";
-let array : Array<string> = [];
-let count = 0;
-for(let i=0 ; i<string2.length ; i++)
-    array[i]=string2.charAt(i);    
-for(let i=0 ; i<string1.length ; i++)
-    for(let j=0 ; j<array.length ; j++)
-        if(string1.charAt(i)==array[j] && array[j]!="")
-        {
-            count++;
-            array[j]="";
-            break;
-        }
-console.log(count);
+let number=9;
+let count=1;
+let count2=2;
+while(count<=number)
+{
+    let aux=0;
+    for (let i = 1; i <= count2; i++)
+        if(count2%i==0)
+            aux++;
+    if(aux==2)
+    {
+        console.log(count2);
+        count++;   
+    }
+    count2++;    
+}
